752beb2cf7a1723f4ebb8de375790350f097f5cf
grubd14 <shinyBroccoli9745@gmail.com>
2023-02-13 19:55:30 +0545

    This commit adds Homescreen.

---
+++ app/src/main/java/com/example/mycity/ui/MyCityHome.kt
@@ -0,0 +1,79 @@
+package com.example.mycity.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycity.R
+
+@Composable
+fun CityHomeScreen() {
+    Card(
+        modifier = Modifier,
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(96.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.img_63),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+
+                    )
+                }
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(4.dp)
+            )
+            Column() {
+                Text(
+                    text = "Hello",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight(550)
+                )
+                Text(
+                    text = "lets see what happens",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            }
+        }
+
+    }
+
+@Preview(showBackground = false)
+@Composable
+fun CityAppHomeScreen() {
+    CityHomeScreen()
+}
